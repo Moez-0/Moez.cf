1876db77323d48a79ab1bb895af454bc024d04dd
Moez-0 <moezlegend@gmail.com>
2024-08-02 16:36:29 +0100

    moooo

---
+++ translations.js
@@ -0,0 +1,33 @@
+// translations.js
+const translations = {
+    en: {
+      help: "Type 'help' to view a list of available commands.",
+      whoami: "What I do",
+      about: "Get to know the idiot behind all of this",
+      skills: "What technologies I use",
+      projects: "The list of projects I made",
+      achievements: "Looking Forward to filling this up :)",
+      website: "My portfolio site but with GUI and a full interface",
+      contact: "Wanna talk to this guy ??",
+      manifesto: "A personal statement to be seen",
+      secret: "Caesar is a title of imperial character. It derives from the cognomen of the Roman dictator Julius Caesar. The name of whom I love most is what uncovers the secrets of it all",
+      clear: "Clears the terminal of all output",
+    },
+    fr: {
+      help: "Tapez 'help' pour voir une liste des commandes disponibles.",
+      whoami: "Ce que je fais",
+      about: "Apprenez à connaître l'idiot derrière tout cela",
+      skills: "Les technologies que j'utilise",
+      projects: "La liste des projets que j'ai réalisés",
+      achievements: "En attendant de le remplir :)",
+      website: "Mon site portfolio avec une interface GUI complète",
+      contact: "Vous voulez parler à ce gars ??",
+      manifesto: "Une déclaration personnelle à voir",
+      secret: "César est un titre de caractère impérial. Il dérive du cognomen du dictateur romain Jules César. Le nom de celui que j'aime le plus est ce qui révèle tous les secrets",
+      clear: "Efface le terminal de toute sortie",
+    },
+    // Add more languages as needed
+  };
+  
+  export default translations;
+  
